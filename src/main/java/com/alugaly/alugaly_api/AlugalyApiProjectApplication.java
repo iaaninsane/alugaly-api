dc725443d4eefa72f3bbfbba0d2b4c04695c286e
ivan.ferreira <iaaninsane@email.com>
2025-05-18 16:10:51 -0300

    primeiro commit

---
+++ src/main/java/com/alugaly/alugaly_api/AlugalyApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alugaly.alugaly_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugalyApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugalyApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alugaly/alugaly_api/AlugalyApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugaly.alugaly_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugalyApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
